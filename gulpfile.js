5dde7e54eef81aeb22fe18c8cd4db47d71aa53c1
srbberlin <info@hans-schrieber.de>
2019-01-22 11:36:52 +0100

    Gulp version 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,32 @@
 const gulp = require('gulp')
 const browserSync = require('browser-sync')
 
+
 var config = {
   base:   __dirname + '/',
   html:   __dirname + '/**/*.html',
   css:    __dirname + '/css/**/*.css',
-  //js:     __dirname + '/js/**/*.js',
-  //img:    __dirname + '/img/**/*'
+  js:     __dirname + '/js/**/*.js',
+  img:    __dirname + '/img/**/*'
 }
 
-gulp.task('reload', function() {
-  console.log('reload')
+function reload () {
+  console.log('reload');
   browserSync.reload()
-})
+}
 
-gulp.task('serve', function() {
+function serve (cp) {
   console.log('serve')
   browserSync({
     server: config.base
   })
 
-  gulp.watch(config.html, ['reload'])
-  gulp.watch(config.css, ['reload'])
-  //gulp.watch(config.js, ['reload'])
-  //gulp.watch(config.img, ['reload'])
-})
+  gulp.watch(config.html, reload)
+  gulp.watch(config.css, reload)
+  gulp.watch(config.js, reload)
+  gulp.watch(config.img, reload)
 
-gulp.task('default', ['serve'])
+  cp();
+}
+
+exports.default = serve
